a6b56f8cc63230e0088288078e4da9433792c7a7
Azmi Azis <azmiazis708@gmail.com>
2026-05-26 01:38:55 +0700

    Migrasi awal dari PHP Native ke Laravel 12 (Sistem Arsip PTPN4)

---
+++ app/Models/Box.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Box extends Model
+{
+    protected $table = 'box';
+    protected $fillable = ['id_pengajuan', 'nomor_box', 'rfid_code', 'keterangan'];
+
+    public function pengajuan(): BelongsTo
+    {
+        return $this->belongsTo(Pengajuan::class, 'id_pengajuan');
+    }
+
+    public function bantex(): HasMany
+    {
+        return $this->hasMany(Bantex::class, 'id_box');
+    }
+}
